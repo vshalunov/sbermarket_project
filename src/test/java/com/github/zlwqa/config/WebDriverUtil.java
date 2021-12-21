628ff03b70f08dfe23bbb36a3b9e325961914509
zlw-qa <asdori95@gmail.com>
2021-12-21 12:27:00 +0400

    please

--- src/test/java/com/github/zlwqa/config/WebDriverUtil.java
+++ src/test/java/com/github/zlwqa/config/WebDriverUtil.java
@@ -38,5 +38,6 @@ public class WebDriverUtil {
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         Configuration.browserCapabilities = capabilities;
         Configuration.timeout=90000;
+        Configuration.pageLoadTimeout = 90000;
     }
 }
